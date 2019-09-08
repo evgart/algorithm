3be0e6a0c5faae652356b0e12b3c765f3c8bc12d
evgart <evgenii-it@mail.ru>
2019-09-08 11:36:22 +0400

    added test

--- benchmark/src/main/java/org/algorithm/benchmark/SortingService.java
+++ benchmark/src/main/java/org/algorithm/benchmark/SortingService.java
@@ -1,5 +1,13 @@
 package org.algorithm.benchmark;
 
 public interface SortingService {
+
     int[] sort(int[] unsortedArray);
+
+    default void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
 }
--- benchmark/src/test/java/org/algorithm/benchmark/SortingServiceImplTest.java
+++ benchmark/src/test/java/org/algorithm/benchmark/SortingServiceImplTest.java
@@ -1,6 +1,7 @@
 package org.algorithm.benchmark;
 
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SortingServiceImplTest {
@@ -8,9 +9,27 @@ class SortingServiceImplTest {
     private SortingService service = new SortingServiceImpl();
     private SortingApp.MyState state = new SortingApp.MyState();
 
-    @org.junit.jupiter.api.Test
+    @Test
     void sort() {
         int[] result = service.sort(state.unsortedArray);
         assertArrayEquals(state.sortedArray, result);
     }
+
+    @Test
+    void sort_simple_with_duplicates() {
+        int[] unsortedArray = {3, 5, 1, 9, 4, 3, 5, 2, 7};
+        int[] sortedArray   = {1, 2, 3, 3, 4, 5, 5, 7, 9};
+        int[] result = service.sort(unsortedArray);
+        assertArrayEquals(sortedArray, result);
+    }
+
+
+    @Test
+    void sort_simple() {
+        int[] unsortedArray = {5, 1, 9, 4, 3, 2, 7};
+        int[] sortedArray   = {1, 2, 3, 4, 5, 7, 9};
+        int[] result = service.sort(unsortedArray);
+        assertArrayEquals(sortedArray, result);
+    }
+
 }
